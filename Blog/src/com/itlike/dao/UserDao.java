edba561ed3ebd5930e86aef5c2b4ed63b52caa34
zhengwu <zhengwu@andrew.cmu.edu>
2019-09-09 15:55:24 -0400

    marc

--- Blog/src/com/itlike/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.itlike.dao;
-
-import com.itlike.domian.User;
-
-public interface UserDao {
-    public User getUser(String username, String password);
-}
--- Blog/src/com/itlike/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.itlike.service;
-
-import com.itlike.domian.User;
-
-public interface LoginService {
-    public User login(User user);
-}
